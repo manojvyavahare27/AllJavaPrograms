38701ab5c1928974731fb8267f0324c46a6cc074
manoj.vyavahare <manoj.vyavahare@riomed.com>
2022-04-09 09:50:36 +0530

    added comment for Checking alphabet

--- Test/src/main/java/JavaPrograms/AlphabetCheck.java
+++ Test/src/main/java/JavaPrograms/AlphabetCheck.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class AlphabetCheck {
 
 	public static void main(String[] args) {
-
+        //Checking entered char is alphabet or not
 		char c='R';
 
 		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
